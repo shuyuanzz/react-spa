28c191929a079d7bac992b2e00f5b11a86f70b53
shuyuan <seanshu96@gmail.com>
2019-11-05 10:54:12 +0800

    add prettierrc in project

--- src/infrastructure/@decorators/mobxProvider.ts
+++ src/infrastructure/@decorators/mobxProvider.ts
@@ -1,9 +1,7 @@
 import React from "react";
 import { Provider } from "mobx-react";
 import StateConstructor from "../interface/stateConstructor";
-export default function mobxProvider(
-  state: StateConstructor
-) {
+export default function mobxProvider(state: StateConstructor) {
   const initialState = new state();
   return (Comp: any): any => {
     class StatusComp extends React.Component {
