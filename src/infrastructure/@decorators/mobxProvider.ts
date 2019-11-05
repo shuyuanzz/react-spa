495d7efe507fa51f19d312dd3b509b9a7bd94eae
shuyuan <seanshu96@gmail.com>
2019-11-05 11:00:49 +0800

    prettier format

--- src/infrastructure/@decorators/mobxProvider.ts
+++ src/infrastructure/@decorators/mobxProvider.ts
@@ -1,18 +1,18 @@
-import React from "react";
-import { Provider } from "mobx-react";
-import StateConstructor from "../interface/stateConstructor";
+import React from 'react'
+import { Provider } from 'mobx-react'
+import StateConstructor from '../interface/stateConstructor'
 export default function mobxProvider(state: StateConstructor) {
-  const initialState = new state();
-  return (Comp: any): any => {
-    class StatusComp extends React.Component {
-      render() {
-        return React.createElement(
-          Provider,
-          { store: initialState },
-          React.createElement(Comp, this.props)
-        );
-      }
-    }
-    return StatusComp;
-  };
+	const initialState = new state()
+	return (Comp: any): any => {
+		class StatusComp extends React.Component {
+			render() {
+				return React.createElement(
+					Provider,
+					{ store: initialState },
+					React.createElement(Comp, this.props)
+				)
+			}
+		}
+		return StatusComp
+	}
 }
--- src/modules/app/app.view.tsx
+++ src/modules/app/app.view.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "../../assets/app.scss";
-import mobxProvider from "../../infrastructure/@decorators/mobxProvider";
-import Appstate from "./states/app.state";
-import HelloReact from "./components/helloReact";
+import React from 'react'
+import '../../assets/app.scss'
+import mobxProvider from '../../infrastructure/@decorators/mobxProvider'
+import Appstate from './states/app.state'
+import HelloReact from './components/helloReact'
 @mobxProvider(Appstate)
 export default class App extends React.Component {
-  render() {
-    return <HelloReact/>
-  }
+	render() {
+		return <HelloReact />
+	}
 }
