1ab04f797f62be953ca229dfdf66ed599f137db9
shuyuan <seanshu96@gmail.com>
2019-11-05 16:58:11 +0800

    add antd in project and framework build

--- src/infrastructure/router/routerComponent.tsx
+++ src/infrastructure/router/routerComponent.tsx
@@ -3,28 +3,18 @@ import { Route, BrowserRouter as Router, Link, Switch } from 'react-router-dom'
 import App from '../../modules/app/app.view'
 import About from '../../modules/app/components/about'
 import Home from '../../modules/app/components/home'
+import Framework from '../framework/framework'
 export default class RouterComponent extends React.Component {
 	render() {
 		return (
 			<Router>
-				<div>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/about">About</Link>
-						</li>
-						<li>
-							<Link to="/home">home</Link>
-						</li>
-					</ul>
-				</div>
-				<Switch>
-					<Route exact path="/" component={App}></Route>
-					<Route path="/about" component={About}></Route>
-					<Route path="/home" component={Home}></Route>
-				</Switch>
+				<Framework>
+					<Switch>
+						<Route exact path="/" component={App}></Route>
+						<Route path="/about" component={About}></Route>
+						<Route path="/home" component={Home}></Route>
+					</Switch>
+				</Framework>
 			</Router>
 		)
 	}
--- src/modules/app/components/helloReact.tsx
+++ src/modules/app/components/helloReact.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { inject, observer } from 'mobx-react'
+import { Button } from 'antd'
 @inject('store')
 @observer
 export default class HelloReact extends React.Component<any, any> {
 	render() {
 		const { store } = this.props
-		return <div>{store.test}</div>
+		return (
+			<div>
+				<Button type="primary">{store.test}</Button>
+			</div>
+		)
 	}
 }
