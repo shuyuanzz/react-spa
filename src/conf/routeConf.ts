5a2477fcf2f6706f0095324dca211554518063f0
shuyuan <seanshu96@gmail.com>
2019-12-04 15:09:03 +0800

    add routeconfig

---
+++ src/conf/routeConf.ts
@@ -0,0 +1,27 @@
+const routeConfig: {
+	name: string
+	component: string
+	path: string
+}[] = [
+	{
+		name: 'app',
+		path: '/',
+		component: 'APP'
+	},
+	{
+		name: 'about',
+		path: '/about',
+		component: 'About'
+	},
+	{
+		name: 'home',
+		path: '/home',
+		component: 'Home'
+	},
+	{
+		name: 'user',
+		path: '/user',
+		component: 'User'
+	}
+]
+export default routeConfig
