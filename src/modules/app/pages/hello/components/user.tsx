6ed087073ab415345123809f3bf7a64e688ef83e
shuyuan <seanshu96@gmail.com>
2019-12-31 16:04:07 +0800

    lint test

--- src/modules/app/pages/hello/components/user.tsx
+++ src/modules/app/pages/hello/components/user.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 export default class User extends React.Component {
-	render()    {
-		   return <div>user</div>
+	render() {
+		return <div>user</div>;
 	}
 }
