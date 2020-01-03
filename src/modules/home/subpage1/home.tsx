72c1a245b2a08cfe5395c71d65a8e0e482f88e84
shuyuan <seanshu96@gmail.com>
2020-01-03 18:09:58 +0800

    add eslint-plugin-react-hooks

--- src/modules/home/subpage1/home.tsx
+++ src/modules/home/subpage1/home.tsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Home() {
+export default function Home(): JSX.Element {
+	const [count, setCount] = useState(0);
+	useEffect(() => {
+		document.title = `you clicked ${count} times`;
+	});
 	return (
-		<div>
-			<ul>
-				<li>
-					<Link to="/home/sub1">sub1</Link>
-				</li>
-				<li>
-					<Link to="/home/sub2">sub2</Link>
-				</li>
-				<li>
-					<Link to="/home/sub3">sub3</Link>
-				</li>
-			</ul>
-		</div>
+		<>
+			<div>
+				<ul>
+					<li>
+						<Link to="/home/sub1">sub1</Link>
+					</li>
+					<li>
+						<Link to="/home/sub2">sub2</Link>
+					</li>
+					<li>
+						<Link to="/home/sub3">sub3</Link>
+					</li>
+				</ul>
+			</div>
+			<button onClick={() => setCount(count + 1)}>{count}</button>
+		</>
 	);
 }
